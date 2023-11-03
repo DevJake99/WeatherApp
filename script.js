a68e4152e8ffad39d206d980bc4ec58e0648a7a2
DevJake99 <jacobg4@uci.edu>
2023-11-03 12:09:46 -0700

    added dynamic html elements for all 5 days of the week and adjusted some css properties

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ let dayFourTemp = $('#dayFourTemp');
 let dayFourWind = $('#dayFourWind');
 
 let dayFiveDiv = $('#dayFive');
+let dayFiveDate = $('#fiveDate');
+let dayFiveTemp = $('#dayFiveTemp');
+let dayFiveWind = $('#dayFiveWind');
 
 // Conversion functions
 function KtoF(Kelvin) {
@@ -73,14 +76,55 @@ function searchCity(city) {
                     console.log('Data', data);
                     //Parse/clean data for the following 5 days
                     //Day 1
-                    tempF = KtoF(data.list[3].main.temp)
+                    let tempF = KtoF(data.list[3].main.temp)
                     let dateTimeOne = data.list[3].dt_txt;
                     let parts = dateTimeOne.split(' ');
                     let dayOne = parts[0].split('-');
                     let dateOne = `${dayOne[1]}/${dayOne[2]}/${dayOne[0]}`;
                     dayOneDate.text(dateOne);
                     dayOneTemp.text(`Temp: ${tempF} °F`);
-                    dayOneWind.text(`Wind: `)
+                    dayOneWind.text(`Wind: ${data.list[3].wind.speed} mi/h`);
+
+                    // Day 2
+                    let tempFTwo = KtoF(data.list[11].main.temp)
+                    let dateTimeTwo = data.list[11].dt_txt;
+                    let partsTwo = dateTimeTwo.split(' ');
+                    let dayTwo = partsTwo[0].split('-');
+                    let dateTwo = `${dayTwo[1]}/${dayTwo[2]}/${dayTwo[0]}`;
+                    dayTwoDate.text(dateTwo);
+                    dayTwoTemp.text(`Temp: ${tempFTwo} °F`);
+                    dayTwoWind.text(`Wind: ${data.list[11].wind.speed} mi/h`);
+
+                    //Day 3
+                    let tempFThree = KtoF(data.list[19].main.temp);
+                    let dateTimeThree = data.list[19].dt_txt;
+                    let partsThree = dateTimeThree.split(' ');
+                    let dayThree = partsThree[0].split('-');
+                    let dateThree = `${dayThree[1]}/${dayThree[2]}/${dayThree[0]}`;
+                    dayThreeDate.text(dateThree);
+                    dayThreeTemp.text(`Temp: ${tempFThree} °F`);
+                    dayThreeWind.text(`Wind: ${data.list[19].wind.speed} mi/h`);
+
+                    //Day 4
+                    let tempFFour = KtoF(data.list[27].main.temp);
+                    let dateTimeFour = data.list[27].dt_txt;
+                    let partsFour = dateTimeFour.split(' ');
+                    let dayFour = partsFour[0].split('-');
+                    let dateFour = `${dayFour[1]}/${dayFour[2]}/${dayFour[0]}`;
+                    dayFourDate.text(dateFour);
+                    dayFourTemp.text(`Temp: ${tempFFour} °F`);
+                    dayFourWind.text(`Wind: ${data.list[27].wind.speed} mi/h`);
+
+                    //Day 5
+                    let tempFFive = KtoF(data.list[36].main.temp);
+                    let dateTimeFive = data.list[36].dt_txt;
+                    let partsFive = dateTimeFive.split(' ');
+                    let dayFive = partsFive[0].split('-');
+                    let dateFive = `${dayFive[1]}/${dayFive[2]}/${dayFive[0]}`;
+                    dayFiveDate.text(dateFive);
+                    dayFiveTemp.text(`Temp: ${tempFFive} °F`);
+                    dayFiveWind.text(`Wind: ${data.list[36].wind.speed} mi/h`);
+
                 })
 
             // }
